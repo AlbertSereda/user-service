6110fc8501be4a710340c88c22e3e80a47ff48c4
Albert Sereda <ASereda@IBS.RU>
2025-02-11 09:53:23 +0500

    refactor config

--- src/main/java/com/market/user/config/AuthConfig.java
+++ src/main/java/com/market/user/config/AuthConfig.java
@@ -6,11 +6,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,17 +19,5 @@ public class AuthConfig {
             .oauth2Login(Customizer.withDefaults())
             .authorizeHttpRequests(r -> r.anyRequest().permitAll());
         return http.build();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("regId")
-                                                                  .clientId("clientId")
-                                                                  .clientSecret("secret")
-                                                                  .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                                                                  .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                                                                  .tokenUri("http://localhost:7070/oauth2/token")
-                                                                  .build();
-        return new InMemoryClientRegistrationRepository(clientRegistration);
     }
 }
